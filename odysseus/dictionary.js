6536e08a49efd92eab7474da0cced5c5c5c3ced0
Sampo Juustila <sampo.juustila@iki.fi>
2019-04-28 16:53:55 +0300

    feat: implement jump drive basic functionality

--- odysseus/dictionary.js
+++ odysseus/dictionary.js
@@ -73,6 +73,153 @@ window.odysseusDictionary = {
             }
         },
         {
+            "name": "Ready countdown",
+            "key": "prop.jumpdrive.readyT",
+            "values": [
+                {
+                    "key": "readyT",
+                    "name": "Regulation jump in",
+                    "format": "string",
+                    "hints": {
+                        "range": 1
+                    }
+                },
+                {
+                    "key": "utc",
+                    "source": "timestamp",
+                    "name": "Timestamp",
+                    "format": "utc",
+                    "hints": {
+                        "domain": 1
+                    }
+                }
+            ],
+            "source": {
+                "method": "backend",
+                "type": "ship",
+                "id": "jumpstate",
+                "field": "readyT",
+            }
+        },
+        {
+            "name": "Ready seconds",
+            "key": "prop.jumpdrive.readyRemaining",
+            "values": [
+                {
+                    "key": "readyRemaining",
+                    "name": "Ready remaining",
+                    "format": "float",
+                    "units": "s",
+                    "hints": {
+                        "range": 1
+                    }
+                },
+                {
+                    "key": "utc",
+                    "source": "timestamp",
+                    "name": "Timestamp",
+                    "format": "utc",
+                    "hints": {
+                        "domain": 1
+                    }
+                }
+            ],
+            "source": {
+                "method": "backend",
+                "type": "ship",
+                "id": "jumpstate",
+                "field": "readyRemaining",
+            }
+        },
+        {
+            "name": "Cooldown countdown",
+            "key": "prop.jumpdrive.cooldownT",
+            "values": [
+                {
+                    "key": "cooldownT",
+                    "name": "Cooldown",
+                    "format": "string",
+                    "hints": {
+                        "range": 1
+                    }
+                },
+                {
+                    "key": "utc",
+                    "source": "timestamp",
+                    "name": "Timestamp",
+                    "format": "utc",
+                    "hints": {
+                        "domain": 1
+                    }
+                }
+            ],
+            "source": {
+                "method": "backend",
+                "type": "ship",
+                "id": "jumpstate",
+                "field": "cooldownT",
+            }
+        },
+        {
+            "name": "Cooldown seconds",
+            "key": "prop.jumpdrive.cooldownRemaining",
+            "values": [
+                {
+                    "key": "cooldownRemaining",
+                    "name": "Cooldown remaining",
+                    "format": "float",
+                    "units": "s",
+                    "hints": {
+                        "range": 1
+                    }
+                },
+                {
+                    "key": "utc",
+                    "source": "timestamp",
+                    "name": "Timestamp",
+                    "format": "utc",
+                    "hints": {
+                        "domain": 1
+                    }
+                }
+            ],
+            "source": {
+                "method": "backend",
+                "type": "ship",
+                "id": "jumpstate",
+                "field": "cooldownRemaining",
+            }
+        },
+        {
+            "name": "Jump countdown",
+            "key": "prop.jumpdrive.jumpT",
+            "values": [
+                {
+                    "key": "jumpT",
+                    "name": "Jump in",
+                    "format": "string",
+                    "hints": {
+                        "range": 1
+                    }
+                },
+                {
+                    "key": "utc",
+                    "source": "timestamp",
+                    "name": "Timestamp",
+                    "format": "utc",
+                    "hints": {
+                        "domain": 1
+                    }
+                }
+            ],
+            "source": {
+                "method": "backend",
+                "type": "ship",
+                "id": "jumpstate",
+                "field": "jumpT",
+            }
+        },
+        {
             "name": "Jump drive state",
             "key": "prop.jumpdrive.state",
             "values": [
@@ -92,28 +239,28 @@ window.odysseusDictionary = {
                         {
                             "value": 2,
                             "string": "Ready to prep",
-                        },
-                        {
+                },
+                {
                             "value": 3,
                             "string": "Calculating",
-                        },
-                        {
+        },
+        {
                             "value": 4,
                             "string": "Preparations",
                         },
-                        {
+                {
                             "value": 5,
                             "string": "Prep complete",
-                        },
-                        {
+                },
+                {
                             "value": 6,
                             "string": "Ready to jump",
-                        },
-                        {
+        },
+        {
                             "value": 7,
                             "string": "Jump initialized",
                         },
-                        {
+                {
                             "value": 8,
                             "string": "Jumping",
                         },
@@ -137,221 +284,7 @@ window.odysseusDictionary = {
                 "type": "ship",
                 "id": "jumpstate",
                 "field": "statusno",
-            }
-        },
-        {
-            "name": "Fuel",
-            "key": "prop.fuel",
-            "values": [
-                {
-                    "key": "value",
-                    "name": "Value",
-                    "units": "kilograms",
-                    "format": "float",
-                    "min": 0,
-                    "max": 100,
-                    "hints": {
-                        "range": 1
-                    }
-                },
-                {
-                    "key": "utc",
-                    "source": "timestamp",
-                    "name": "Timestamp",
-                    "format": "utc",
-                    "hints": {
-                        "domain": 1
-                    }
-                }
-            ],
-            "source": {
-                "method": "random_brownian",
-                "brownianMultiplier": 0.1,
-                "initialValue": 0
-            }
-        },
-        {
-            "name": "Thrusters",
-            "key": "prop.thrusters",
-            "values": [
-                {
-                    "key": "value",
-                    "name": "Value",
-                    "format": "enum",
-                    "enumerations": [
-                        {
-                            "string": "ON",
-                            "value": 1
-                        },
-                        {
-                            "string": "OFF",
-                            "value": 0
-                        }
-                    ],
-                    "hints": {
-                        "range": 1
-                    }
-                },
-                {
-                    "key": "utc",
-                    "source": "timestamp",
-                    "name": "Timestamp",
-                    "format": "utc",
-                    "hints": {
-                        "domain": 1
-                    }
-                }
-            ],
-            "source": {
-                "method": "random_brownian",
-                "brownianMultiplier": 0.1,
-                "initialValue": 0
-            }
-        },
-        {
-            "name": "Received",
-            "key": "comms.recd",
-            "values": [
-                {
-                    "key": "value",
-                    "name": "Value",
-                    "units": "bytes",
-                    "format": "integer",
-                    "hints": {
-                        "range": 1
-                    }
-                },
-                {
-                    "key": "utc",
-                    "source": "timestamp",
-                    "name": "Timestamp",
-                    "format": "utc",
-                    "hints": {
-                        "domain": 1
-                    }
-                }
-            ],
-            "source": {
-                "method": "random_brownian",
-                "brownianMultiplier": 0.1,
-                "initialValue": 0
-            }
-        },
-        {
-            "name": "Sent",
-            "key": "comms.sent",
-            "values": [
-                {
-                    "key": "value",
-                    "name": "Value",
-                    "units": "bytes",
-                    "format": "integer",
-                    "hints": {
-                        "range": 1
-                    }
-                },
-                {
-                    "key": "utc",
-                    "source": "timestamp",
-                    "name": "Timestamp",
-                    "format": "utc",
-                    "hints": {
-                        "domain": 1
-                    }
-                }
-            ],
-            "source": {
-                "method": "random_brownian",
-                "brownianMultiplier": 0.1,
-                "initialValue": 0
-            }
-        },
-        {
-            "name": "Generator Temperature",
-            "key": "pwr.temp",
-            "values": [
-                {
-                    "key": "value",
-                    "name": "Value",
-                    "units": "℃",
-                    "format": "float",
-                    "hints": {
-                        "range": 1
-                    }
-                },
-                {
-                    "key": "utc",
-                    "source": "timestamp",
-                    "name": "Timestamp",
-                    "format": "utc",
-                    "hints": {
-                        "domain": 1
-                    }
-                }
-            ],
-            "source": {
-                "method": "random_brownian",
-                "brownianMultiplier": 0.1,
-                "initialValue": 60
-            }
-        },
-        {
-            "name": "Generator Current",
-            "key": "pwr.c",
-            "values": [
-                {
-                    "key": "value",
-                    "name": "Value",
-                    "units": "Amps",
-                    "format": "float",
-                    "hints": {
-                        "range": 1
-                    }
-                },
-                {
-                    "key": "utc",
-                    "source": "timestamp",
-                    "name": "Timestamp",
-                    "format": "utc",
-                    "hints": {
-                        "domain": 1
-                    }
-                }
-            ],
-            "source": {
-                "method": "random_brownian",
-                "brownianMultiplier": 0.01,
-                "initialValue": 0.5
-            }
-        },
-        {
-            "name": "Generator Voltage",
-            "key": "pwr.v",
-            "values": [
-                {
-                    "key": "value",
-                    "name": "Value",
-                    "units": "Volts",
-                    "format": "float",
-                    "hints": {
-                        "range": 1
-                    }
-                },
-                {
-                    "key": "utc",
-                    "source": "timestamp",
-                    "name": "Timestamp",
-                    "format": "utc",
-                    "hints": {
-                        "domain": 1
-                    }
-                }
-            ],
-            "source": {
-                "method": "random_brownian",
-                "brownianMultiplier": 0.01,
-                "initialValue": 12
-            }
         }
+        },
     ]
 }
